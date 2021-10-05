6770b2fa4fbd4cf91286aa4ef92375aed252a4c1
kahiga <kahigakamiru@gmail.com>
2021-10-05 17:25:09 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -0.4270885, lng: 36.9455516 },
+    zoom: 8,
+  });
+}
+
+
+// AIzaSyBWO3t95xK7zUaV4_0pS0JUryjP9wtplms
